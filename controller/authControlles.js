a594bfe5aaaf0bf2106ab75cadc62116d91fda8f
hahosameer <ks4063180@gmail.com>
2024-09-06 14:43:15 +0500

    login api

--- controller/authControlles.js
+++ controller/authControlles.js
@@ -1,6 +1,7 @@
 import { genSaltSync, hashSync } from "bcrypt";
 import { generateToken } from "../middleware/verifyToken.js";
 import User from "../models/User.js"
+import bcrypt from "bcrypt";
 export const signUp = async (req, res) => {
   try {
     const { username, email, password } = req.body;
@@ -55,3 +56,32 @@ export const signUp = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+
+export const login = async (req, res) => {
+      const { email, password } = req.body;
+try {
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  // Compare password with hashed password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  // Generate token after successful login
+  const token = generateToken({ data: { id: user._id, email: user.email } });
+
+        return res.status(200).json({ message: "Login successful", token });
+} catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+}
+}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,4 +1,5 @@
 import express from "express";
-import { signUp } from "../controller/authControlles.js";
+import { login, signUp } from "../controller/authControlles.js";
 export const authRoutes = express.Router();
 authRoutes.post("/signup", signUp);
+authRoutes.post("/login", login);
